4debf7ed9a13888bd626790500abd096c5598b28
Mario <msiu001@fiu.edu>
2018-03-30 02:22:54 -0400

    adding the register form in the register.component.html with the style, logo and submmit button and the style.css for the submit button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './register/register.component';
@@ -20,7 +21,9 @@ import { appRoutes } from './routes';
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    FormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]

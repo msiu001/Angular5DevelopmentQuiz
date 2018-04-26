89bbbd736fcc2dee0bfb1a0c839b11169f373949
Mario <msiu001@fiu.edu>
2018-04-26 13:09:15 -0400

    this is to avoid the loss of data when the results are set and the page is reload, the score, participant name and time are not lost

--- src/app/result/result.component.ts
+++ src/app/result/result.component.ts
@@ -1,41 +1,46 @@
-import { Component, OnInit } from '@angular/core';
-import { QuizService } from '../shared/quiz.service';
-import { Router } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { QuizService } from "../shared/quiz.service";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-result',
-  templateUrl: './result.component.html',
-  styleUrls: ['./result.component.css']
+  selector: "app-result",
+  templateUrl: "./result.component.html",
+  styleUrls: ["./result.component.css"]
 })
 export class ResultComponent implements OnInit {
-
-  constructor( private quizService: QuizService, private router: Router) { }
+  constructor(private quizService: QuizService, private router: Router) {}
 
   ngOnInit() {
-    this.quizService.getAnswers().subscribe(
-      (data : any) => {
+    if (parseInt(localStorage.getItem("qnProgress")) == 10) {
+      //this if is to avoid the restart of the quiz due to reload of page
+      this.quizService.seconds = parseInt(localStorage.getItem("seconds"));
+      this.quizService.qnProgress = parseInt(
+        localStorage.getItem("qnProgress")
+      );
+      this.quizService.qns = JSON.parse(localStorage.getItem("qns"));
+
+      this.quizService.getAnswers().subscribe((data: any) => {
         this.quizService.correctAnswersCount = 0;
-        this.quizService.qns.forEach((e,i) => {
-          if(e.answer == data[i])
-          this.quizService.correctAnswersCount++;            
+        this.quizService.qns.forEach((e, i) => {
+          if (e.answer == data[i]) this.quizService.correctAnswersCount++;
           e.correct = data[i];
         });
-      }
-    );
+      });
+    } else {
+      this.router.navigate(['/quiz'])
+    }
   }
 
-
-  OnSubmit(){
+  OnSubmit() {
     this.quizService.submitScore().subscribe(() => {
       this.restart();
     });
   }
 
-
-  restart(){
-    this.router.navigate(['/quiz']);
+  restart() {
+    localStorage.setItem('qnProgress', "0");
+    localStorage.setItem('seconds', "0");
+    localStorage.setItem('qns', "");
+    this.router.navigate(["/quiz"]);
   }
-
-
-
 }
